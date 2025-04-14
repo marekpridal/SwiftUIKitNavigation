915a27c627421395725f8ce7e3579972c6794abd
Marek Pridal <marek@marekpridal.eu>
2025-04-14 16:15:34 +0200

    Merge Coordinator and CoordinatorWithDidFinish

--- Sources/Navigation/Coordinator.swift
+++ Sources/Navigation/Coordinator.swift
@@ -2,22 +2,33 @@ import UIKit
 
 @MainActor
 public protocol Coordinator: AnyObject, ObservableObject {
+    // MARK: Associated types
     associatedtype Input
+    associatedtype Output
 
+    // MARK: Variables
     var childCoordinators: [any Coordinator] { get set }
-    var input: Input? { get }
 
-    init(navigationController: UINavigationController, input: Input?)
+    // MARK: Constants
+    var input: Input? { get }
+    var navigationController: UINavigationController { get }
+    var showCloseButton: Bool { get }
+    var coordinatorDidFinish: ((Coordinator, Output?) -> Void)? { get }
+
+    // MARK: Init
+    init(navigationController: UINavigationController, input: Input?, showCloseButton: Bool, coordinatorDidFinish: ((Coordinator, Output?) -> Void)?)
 }
 
+// MARK: - Lifecycle management
 extension Coordinator {
-    /// Add a child coordinator to the parent
-    private func addChildCoordinator(childCoordinator: any Coordinator) {
+    /// Adds a child coordinator to the parent
+    public func addChildCoordinator(childCoordinator: any Coordinator) {
         print("adding child coordinator: \(childCoordinator)")
         childCoordinators.append(childCoordinator)
         print("\(String(describing: self)) childrens are now: \(childCoordinators.map { String(describing: $0) }.joined(separator: ", "))")
     }
 
+    /// Adds a child coordinator to the parent and removes the same type in case it already exists
     public func addChildCoordinatorAndRemoveIfSameTypeExists(childCoordinator: any Coordinator) {
         let toRemove = childCoordinators.first { coord -> Bool in
             type(of: coord) == type(of: childCoordinator)
@@ -36,8 +47,56 @@ extension Coordinator {
         print("\(String(describing: self)) childrens are now: \(childCoordinators.map { String(describing: $0) }.joined(separator: ", "))")
     }
 
+    /// Remove a child coordinator from the parent
     public func removeChildCoordinator<T: Coordinator>(ofType type: T.Type) {
         guard let childCoordinator = childCoordinators.getChild(type: type) else { return }
         removeChildCoordinator(childCoordinator: childCoordinator)
+    }
+}
+
+// MARK: - Buttons
+extension Coordinator {
+    /// Setups back button for bridging between SwiftUI and UIKit navigation stack
+    public func setupBackButton() {
+        guard showCloseButton == false else {
+            return
+        }
+
+        guard let coordinatorDidFinish else {
+            navigationController.topViewController?.navigationItem.leftBarButtonItem = nil
+            return
+        }
+
+        var configuration = UIButton.Configuration.plain()
+        configuration.imagePadding = 8
+
+        let backButton = UIButton(configuration: configuration, primaryAction: UIAction { [weak self] _ in
+            guard let self else { return }
+            coordinatorDidFinish(self, nil)
+        })
+        let backButtonImage = UIImage(systemName: "chevron.backward")?.applyingSymbolConfiguration(UIImage.SymbolConfiguration(weight: .semibold))?.withAlignmentRectInsets(UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0))
+        backButton.setImage(backButtonImage, for: .normal)
+        backButton.setTitle("Back", for: .normal)
+        backButton.setTitleColor(backButton.tintColor, for: .normal)
+
+        navigationController.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(customView: backButton)
+    }
+
+    /// Setups close button for modal presentation
+    public func setupCloseButton() {
+        guard let coordinatorDidFinish else {
+            navigationController.topViewController?.navigationItem.leftBarButtonItem = nil
+            return
+        }
+
+        guard showCloseButton else {
+            setupBackButton()
+            return
+        }
+
+        navigationController.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(systemItem: .close, primaryAction: UIAction { [weak self] _ in
+            guard let self else { return }
+            coordinatorDidFinish(self, nil)
+        })
     }
 }
--- Sources/Navigation/NavigationControllerView.swift
+++ Sources/Navigation/NavigationControllerView.swift
@@ -1,12 +1,12 @@
 import SwiftUI
 
-public struct NavigationControllerView<C: Coordinator, Output>: View {
+public struct NavigationControllerView<C: Coordinator>: View {
     private let appCoordinator: any Coordinator
     private let input: C.Input?
-    private let coordinatorDidFinish: ((CoordinatorOutput<Output>) -> Void)?
+    private let coordinatorDidFinish: ((any Coordinator, C.Output?) -> Void)?
     private let showCloseButton: Bool
 
-    public init(appCoordinator: any Coordinator, input: C.Input? = nil, showCloseButton: Bool = false, coordinatorDidFinish: ((CoordinatorOutput<Output>) -> Void)? = nil) {
+    public init(appCoordinator: any Coordinator, input: C.Input? = nil, showCloseButton: Bool = false, coordinatorDidFinish: ((any Coordinator, C.Output?) -> Void)? = nil) {
         self.appCoordinator = appCoordinator
         self.input = input
         self.coordinatorDidFinish = coordinatorDidFinish
@@ -14,27 +14,21 @@ public struct NavigationControllerView<C: Coordinator, Output>: View {
     }
 
     public var body: some View {
-        _NavigationControllerView<C, Output>(appCoordinator: appCoordinator, input: input, showCloseButton: showCloseButton, coordinatorDidFinish: coordinatorDidFinish)
+        _NavigationControllerView<C>(appCoordinator: appCoordinator, input: input, showCloseButton: showCloseButton, coordinatorDidFinish: coordinatorDidFinish)
             .ignoresSafeArea()
             .toolbar(.hidden, for: .navigationBar)
     }
 }
 
-private struct _NavigationControllerView<C: Coordinator, Output>: UIViewControllerRepresentable {
+private struct _NavigationControllerView<C: Coordinator>: UIViewControllerRepresentable {
     let appCoordinator: any Coordinator
     let input: C.Input?
     let showCloseButton: Bool
-    let coordinatorDidFinish: ((CoordinatorOutput<Output>) -> Void)?
+    let coordinatorDidFinish: ((any Coordinator, C.Output?) -> Void)?
 
     func makeUIViewController(context: Context) -> UINavigationController {
         let navigationController = UINavigationController()
-        let coordinator = C(navigationController: navigationController, input: input)
-        /*
-        if let coordinatorWithDidFinish = coordinator as? any CoordinatorWithDidFinish {
-            coordinatorWithDidFinish.coordinatorDidFinish = coordinatorDidFinish
-            coordinatorWithDidFinish.showCloseButton = showCloseButton
-        }
-        */
+        let coordinator = C(navigationController: navigationController, input: input, showCloseButton: showCloseButton, coordinatorDidFinish: coordinatorDidFinish)
         appCoordinator.addChildCoordinatorAndRemoveIfSameTypeExists(childCoordinator: coordinator)
         return navigationController
     }
